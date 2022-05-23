e795bd8ec93342356e3ee727a0510ce08301b085
Nazar Ostafiichuk <90191817+Ostafii4uk@users.noreply.github.com>
2022-05-23 13:18:18 +0300

    Updates

---
+++ precache-manifest.19d3c85be524d78aee50f1511080453f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5b866b9541a8c2b68a9",
+    "url": "/auto-Complete-Boilerplate/static/css/main.7676a839.chunk.css"
+  },
+  {
+    "revision": "a5b866b9541a8c2b68a9",
+    "url": "/auto-Complete-Boilerplate/static/js/main.a5b866b9.chunk.js"
+  },
+  {
+    "revision": "982b5fbb18df613851c9",
+    "url": "/auto-Complete-Boilerplate/static/js/runtime~main.982b5fbb.js"
+  },
+  {
+    "revision": "7bd247e6073681a1ecc5",
+    "url": "/auto-Complete-Boilerplate/static/js/2.7bd247e6.chunk.js"
+  },
+  {
+    "revision": "51015f0980c3d29c67275ab6b3598a12",
+    "url": "/auto-Complete-Boilerplate/index.html"
+  }
+];
